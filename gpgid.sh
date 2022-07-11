22a1464ac70e7d6a4c9fba4bb63e62889e92f252
John Waller <john@johnwaller.us>
2022-07-10 23:22:41 -0500

    usable version

--- gpgid.sh
+++ gpgid.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
-function idssh() {
-   gpg --list-keys --with-keygrip $1 | awk '/\[A\]/ { f = 1; next } f && /Keygrip/ { print $3 } { f = 0 }' > $GNUPGHOME/sshcontrol
+function gpgid() {
+	read -r -d '' script <<-'EOF'
+/\[A\]/ { f = 1; next }
+f && /Keygrip/ {
+	print $3 > ENVIRON["GNUPGHOME"] "/sshcontrol"
 }
-idssh $@
+{ f = 0 }
+
+sub(/^uid.*\] /, "", $0) {
+	email = substr($(NF), 2, length($(NF))-2)
+	NF-=1
+	system("git config --global user.email " email)
+	system("git config --global user.name '" $0 "'")
+}
+EOF
+
+	gpg --list-keys --with-keygrip $1 | awk "$script"
+}
+gpgid $@
